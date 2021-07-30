16e8991d9b0c3fbf231db903015f4229c85a2199
praneetk77 <praneetkarna77@gmail.com>
2021-07-30 23:06:18 +0545

    Added a BMI calculator

--- calculator.js
+++ calculator.js
@@ -10,8 +10,19 @@ app.get("/", function(req,res){
 app.post("/", function(req,res){
     var ans = Number(req.body.num1) + Number(req.body.num2);
     res.send("Answer is " + ans);
-})
+});
+
+app.get("/bmicalculator",function(req,res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator",function(req,res){
+    var weight = req.body.weight;
+    var height = req.body.height;
+    var bmi = Math.floor(weight/Math.pow(height,2));
+    res.send("Tera bmi hai " + bmi);
+});
 
 app.listen(3000,function(){
     console.log("Calculator server started on port 3000");
-})
+});
